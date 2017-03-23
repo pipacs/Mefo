f3744480fefb3fda29c00fb29e1ac1581af30c6b
Akos Polster <ako@unwire.dk>
2017-03-23 14:47:39 +0100

    Forward messages.

--- app/src/main/java/com/pipacs/mefo/Forwarder.java
+++ app/src/main/java/com/pipacs/mefo/Forwarder.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.provider.Telephony;
 import android.support.annotation.Nullable;
+import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.util.Log;
+import java.util.ArrayList;
 
 public class Forwarder extends Service {
     static final String TAG = "Forwarder";
@@ -33,13 +35,25 @@ public class Forwarder extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i(TAG, "onStartCommand: " + intent.getAction());
+        String destination = ""; // FIXME: Get it from preferences
+        SmsManager smsManager = SmsManager.getDefault();
         SmsMessage[] messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
         for (SmsMessage message: messages) {
-            String phoneNumber = message.getDisplayOriginatingAddress();
+            String phoneNumber = message.getOriginatingAddress();
             String body = message.getDisplayMessageBody();
-            Log.i(TAG, "handleSmsReceived: From: " + phoneNumber + "; Message: " + body);
+            if (body == null) {
+                body = "(no text)";
+            }
+            Log.i(TAG, "onStartCommand: From: " + phoneNumber + "; Message: " + body);
             //Toast toast = Toast.makeText(context, "Sender: " + phoneNumber + ", Message: " + body, Toast.LENGTH_LONG);
             //toast.show();
+            String forwardedMessage = ">> From " + phoneNumber + "\n" + body;
+            ArrayList<String> forwardedParts = smsManager.divideMessage(forwardedMessage);
+            try {
+                smsManager.sendMultipartTextMessage(destination, phoneNumber, forwardedParts, null, null);
+            } catch (Exception e) {
+                Log.e(TAG, "onStartCommand: " + e.toString());
+            }
         }
         return START_NOT_STICKY;
     }
